67be0c97dc710a73d7be22904163c390525b52a0
C. Titus Brown <titus@idyll.org>
2024-05-04 12:14:42 -0700

    ???

--- scripts/classify-hashes.py
+++ scripts/classify-hashes.py
@@ -1,5 +1,4 @@
 #! /usr/bin/env python
-
 import sys
 import csv
 import argparse
@@ -7,6 +6,7 @@ import sourmash
 from collections import defaultdict
 import pprint
 
+from sourmash.save_load import SaveSignaturesToLocation
 
 from pangenome_elements import CENTRAL_CORE, EXTERNAL_CORE, SHELL, \
     INNER_CLOUD, SURFACE_CLOUD, NAMES
@@ -26,6 +26,9 @@ def main():
     sketch = sketches[0]
     minhash = sketch.minhash
     hashes = minhash.hashes
+
+    central_core_mh = minhash.copy_and_clear()
+    shell_mh = minhash.copy_and_clear()
 
     for csv_file in args.classify_csv_files:
         with open(csv_file, 'r', newline='') as fp:
@@ -47,32 +50,30 @@ def main():
             counter_d[classify] += 1
             if classify >= 0:
                 total_classified += 1
+            if classify == CENTRAL_CORE:
+                central_core_mh.add_hash(hashval)
+            elif classify == SHELL:
+                shell_mh.add_hash(hashval)
+                
 
         print(f"For '{csv_file}', signature '{sketch.name}' contains:")
         for int_id in sorted(NAMES):
             name = NAMES[int_id]
-            count = counter_d.get(int_id)
-            if count is None:
-                raise Exception("Count is None, check `-k` value matches in both files")
+            count = counter_d.get(int_id, 0)
             percent = count / total_classified * 100
             print(f"\t {count} ({percent:.1f}%) hashes are classified as {name}")
 
-        count = counter_d.get(-1)
-        pprint.pprint(count)
-        classified_total_count = 0
-        for (k,v) in counter_d.items():
-            if k >= 0:
-                classified_total_count += v
+        count = counter_d.get(-1, 0)
+        print(f"\t ...and {count} hashes are NOT IN the csv file")
 
-        total_count = 0
-        for (k,v) in counter_d.items(): total_count += v 
-        assert total_count > 0
-
-        percent = count / total_count * 100
-
-        print(f"\t ...and {classified_total_count} ({classified_total_count/total_count*100:.1f}%) hashes are in the {csv_file}")
-        print(f"\t ...and {count} ({percent:.1f}%) hashes are NOT IN the {csv_file}")
+        core_ss = sourmash.SourmashSignature(central_core_mh, name='core')
+        with SaveSignaturesToLocation('core.sig.gz') as save_sig:
+            save_sig.add(core_ss)
+        shell_ss = sourmash.SourmashSignature(shell_mh, name='shell')
+        with SaveSignaturesToLocation('shell.sig.gz') as save_sig:
+            save_sig.add(shell_ss)
 
 
 if __name__ == '__main__':
     sys.exit(main())
+
